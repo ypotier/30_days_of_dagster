27d6e98c4221f942c4dfc92af4ac140d4dce0adc
Sean Lopp <lopp@elementl.com>
2024-11-26 09:49:35 -0700

    day 5

---
+++ solutions/day05.py
@@ -0,0 +1,22 @@
+from dagster import asset   
+
+# https://youtu.be/jNCaAYA-MVU
+
+import pandas as pd
+
+@asset 
+def bike_raw():
+    df = pd.read_csv('solutions/bike_info.csv')
+    return df
+
+@asset(
+    deps=[bike_raw]
+) 
+def b(): ... 
+
+@asset(
+    deps=[b]
+) 
+def c(): ...
+
+bike_raw()
